dab3e3fe2f9dbc8e73037fa81383fd1a49504599
devgunho <devgun@kakao.com>
2020-06-15 22:03:40 +0900

    commit

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 ---
 
-**Elements of Synchronization**
+- **Elements of Synchronization**
 
 Scheduling processes, Kernel objects, Threads, Thread scheduling
 Thread Programming Pattern
@@ -17,9 +17,67 @@ Thread Programming Pattern
 >2. ThreadExcept
 >3. MyThread
 
+<br/>
 
+- **thread synchronization**
 
 The Meaning and Necessity of Synchronicization from the Perspective of **Atomicity** and **Instruction Reordering**
 
 >4. InterLock
 >5. MemBarrier
+
+<br/>
+
+<br/>
+
+### Synchronize with Kernel Objects
+
+**Describes the sync wait functions and sync-only kernel objects provided by Windows**
+
+**Usefulness and utilization of threads or window messages**
+
+-----
+
+- **Thread Synchronization API**
+
+Description of the standby function that waits for threads to synchronize
+
+Understand the relationship between the standby function and the kernel object
+
+> 6.WaitThread
+>
+> 7.MyThread_ModifyWait
+>
+> 8.WaitMultiThreads
+
+<br/>
+
+- **Synchronization objects for data protection**
+
+Describe Mutex and Semaphore from the perspective of protection of shared resources
+
+> 9.MutexTest
+>
+> 10.MutexNoti
+>
+> 11.SemaphoreTest
+>
+> 12.TPSemaphore
+>
+> 13.WQSemaphore
+
+<br/>
+
+- **Synchronization object for flow control**
+
+Description of synchronization-only objects for the purpose of controlling and notifying flows between threads.
+
+> 14.ExitWithEvent
+>
+> 15.MyThread(3)_ModifyExit
+>
+> 16.EventTest
+>
+> 17.MultiSyncWaits
+>
+> 18.EventNotify
--- WindowsSystemProgramming/18.EventNotify/EventNotify.cpp
+++ WindowsSystemProgramming/18.EventNotify/EventNotify.cpp
@@ -54,29 +54,33 @@ DWORD WINAPI WorkerProc(LPVOID pParam)
 
 		switch (lCmd)
 		{
-			case CMD_STR:
-			{
-				pData[lSize] = 0;
-				printf("  <== R-TH %d read STR : %s\n", dwThrId, pData);
-			}
-			break;
-			
-			case CMD_POINT:
-			{
-				PPOINT ppt = (PPOINT)pData;
-				printf("  <== R-TH %d read POINT : (%d, %d)\n", dwThrId, ppt->x, ppt->y);
-			}
-			break;
-			
-			case CMD_TIME:
-			{
-				PSYSTEMTIME pst = (PSYSTEMTIME)pData;
-				printf("  <== R-TH %d read TIME : %04d-%02d-%02d %02d:%02d:%02d+%03d\n",
-					dwThrId, pst->wYear, pst->wMonth, pst->wDay, pst->wHour,
-					pst->wMinute, pst->wSecond, pst->wMilliseconds);
-			}
-			break;
+			// 문자열 데이터에 대한 명령 처리
+		case CMD_STR:
+		{
+			pData[lSize] = 0;
+			printf("  <== R-TH %d read STR : %s\n", dwThrId, pData);
 		}
+		break;
+
+		// POINT 구조체 형에 대한 명령 처리
+		case CMD_POINT:
+		{
+			PPOINT ppt = (PPOINT)pData;
+			printf("  <== R-TH %d read POINT : (%d, %d)\n", dwThrId, ppt->x, ppt->y);
+		}
+		break;
+
+		// SYSTEMTIME 구조체 형에 대한 명령 처리
+		case CMD_TIME:
+		{
+			PSYSTEMTIME pst = (PSYSTEMTIME)pData;
+			printf("  <== R-TH %d read TIME : %04d-%02d-%02d %02d:%02d:%02d+%03d\n",
+				dwThrId, pst->wYear, pst->wMonth, pst->wDay, pst->wHour,
+				pst->wMinute, pst->wSecond, pst->wMilliseconds);
+		}
+		break;
+		}
+		// 공유 버퍼로부터 데이터를 복사한 임시 버퍼를 삭제한다.
 		delete[] pData;
 	}
 	cout << " *** WorkerProc Thread exits..." << endl;
@@ -87,30 +91,39 @@ DWORD WINAPI WorkerProc(LPVOID pParam)
 void _tmain()
 {
 	cout << "======= Start EventNotify Test ========" << endl;
+	cout << "  [1] \"time\"" << endl;
+	cout << "  [2] \"point\"" << endl;
+	cout << "  [3] Other User Input String" << endl;
+	cout << "  [4] \"quit\"" << endl << endl;
 
 	WAIT_ENV we;
 	we._hevSend = CreateEvent(NULL, FALSE, FALSE, NULL);
 	we._hevResp = CreateEvent(NULL, FALSE, FALSE, NULL);
+	// 쓰기 완료 이벤트와 읽기 완료 이벤트를 각각 자동 리셋 이벤트로 생성한다.
 
 	DWORD  dwThrID;
 	HANDLE hThread = CreateThread(NULL, 0, WorkerProc, &we, 0, &dwThrID);
+	// 스레드를 생성하고 WAIT_ENV의 포인터를 매개변수로 넘긴다.
 
 	char szIn[512];
 	while (true)
 	{
 		cin >> szIn;
+		// 콘솔로부터 입력받은 문자열이 "quit"일 경우 프로그램 종료를 위해 루프를 탈출한다.
 		if (_stricmp(szIn, "quit") == 0)
 			break;
 
 		LONG lCmd = CMD_NONE, lSize = 0;
 		PBYTE pIter = we._arBuff + sizeof(LONG) * 2;
-		
+		// 공유 버퍼 구성을 위해서 데이터를 먼저 채우기 위한 포인터를 지정한다.
+
 		if (_stricmp(szIn, "time") == 0)
 		{
 			SYSTEMTIME st;
 			GetLocalTime(&st);
 			memcpy(pIter, &st, sizeof(st));
 			lCmd = CMD_TIME, lSize = sizeof(st);
+			// "time"인 경우 현재 시간을 구해서 공유 버퍼에 복사하고, 명령 종류와 SYSTEMTIME 구조체의 크기를 설정한다.
 		}
 		else if (_stricmp(szIn, "point") == 0)
 		{
@@ -118,21 +131,28 @@ void _tmain()
 			pt.x = rand() % 1000; pt.y = rand() % 1000;
 			*((PPOINT)pIter) = pt;
 			lCmd = CMD_POINT, lSize = sizeof(pt);
+			// "point"인 경우 현재 난수를 통해 x,y 좌표를 구해서 POINT 구조체에 대입한 후 공유 버퍼에 쓴다.
+			// 또한 명령 종류와 POINT 구조체의 크기를 설정한다.
 		}
 		else
 		{
 			lSize = strlen(szIn);
 			memcpy(pIter, szIn, lSize);
 			lCmd = CMD_STR;
+			// 문자열인 경우 문자열의 길이를 얻어 그 길이만큼 공유 버퍼에 복사한 후 CMD_STR 명령을 설정한다.
 		}
 		((PLONG)we._arBuff)[0] = lCmd;
 		((PLONG)we._arBuff)[1] = lSize;
+		// 공유 버퍼의 맨 앞 부분에 명령 종류와 데이터 길이를 설정한다.
+		// 이것으로 공유 버퍼 구성이 마무리되었으므로 이제 쓰기 완료 통지를 해야 한다.
 
 		SignalObjectAndWait(we._hevSend, we._hevResp, INFINITE, FALSE);
+		// 쓰기 완료 통지와 더불어 읽기 완료 통지를 위해 대기한다.
 	}
 
 	*((PLONG)we._arBuff) = CMD_EXIT;
 	SignalObjectAndWait(we._hevSend, hThread, INFINITE, FALSE);
+	// 명령 처리 스레드가 종료되기를 기다린다.
 	CloseHandle(hThread);
 
 	CloseHandle(we._hevResp);
